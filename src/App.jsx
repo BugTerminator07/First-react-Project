f3fdbe3dbed94df07cea0f3b2fdcda8009d6aa31
BugTerminator07 <republicofgamerssakin@gmail.com>
2026-04-14 10:15:15 +0600

    begin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Countries from './components/countries/countries'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>HEllo React</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
